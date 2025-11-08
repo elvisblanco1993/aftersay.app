079c3aae321dfd2f848f9a8cb42d53eb50fef24f
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2025-11-07 19:37:43 -0500

    WIP: Wordpress Testimonials plugin

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WordpressSyncController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/contacts', [ContactController::class, 'store']);
+
+    Route::get('/testimonials/{tenant}', [WordpressSyncController::class, 'index']);
 
     Route::get('/me', function (Request $request) {
         return response()->json([
